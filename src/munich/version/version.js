11ad4f4b3f046565136a8b04f400cbe48db8fb8b
TechAppsSv <techappsv@hotmail.com>
2021-12-04 19:29:03 -0600

    2.1 beta

---
+++ src/munich/version/version.js
@@ -0,0 +1,10 @@
+// The Munich Proyect
+
+
+
+var text2 = document.createTextNode("Electron 16.0.0");                                    
+document.getElementById("electron-version-munich").appendChild(text2); 
+var text3 = document.createTextNode("1.2 Stable");                                    
+document.getElementById("munich-version").appendChild(text3); 
+var text5 = document.createTextNode("2.1 Beta ");                                    
+document.getElementById("version").appendChild(text5); 
