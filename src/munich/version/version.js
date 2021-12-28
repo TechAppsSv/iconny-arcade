69c35168c4039c660ac5dcfc3f6fdafc07d51663
TechAppsSv <techappsv@hotmail.com>
2021-12-28 14:26:02 -0600

    First public version

--- src/munich/version/version.js
+++ src/munich/version/version.js
@@ -6,5 +6,5 @@ var text2 = document.createTextNode("Electron 16.0.0");
 document.getElementById("electron-version-munich").appendChild(text2); 
 var text3 = document.createTextNode("1.2 Stable");                                    
 document.getElementById("munich-version").appendChild(text3); 
-var text5 = document.createTextNode("2.1 Beta ");                                    
+var text5 = document.createTextNode("2 Stable ");                                    
 document.getElementById("version").appendChild(text5); 
